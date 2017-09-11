c42125f44477f108e06b2a8e426cdf10228b59f8
Mnguyen8 <mnguyen8@hawk.iit.edu>
2017-09-11 15:31:33 -0500

    the jQuery for the selectors.html file

--- Week 3/selectors.js
+++ Week 3/selectors.js
@@ -1,1 +1,12 @@
+var main = function(){
 
+	$("div p:first-child").css("color","red");
+	$("div p:nth-child(2)").css("color", "orange");	
+	$("div p:nth-child(3)").css("color", "yellow");
+	$("div p:nth-child(4)").css("color", "green");	
+	$("div p:nth-child(5)").css("color", "blue");
+	$("div p:nth-child(6)").css("color", "purple");
+	$("div p:last-child").css("color", "pink");
+}
+
+$(document).ready(main);
