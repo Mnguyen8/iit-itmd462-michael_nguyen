2696d21d2923a7f2258d5174c14a4f1c0ff4bc9c
Mnguyen8 <mnguyen8@hawk.iit.edu>
2017-09-11 16:32:56 -0500

    fizzbuzz.js
    Takes user input, converts to a integer, makes sure that the user entered a number that is greater or equal to 1, checks the remainder of the number to see if it is equal to 0 (making that number a multiple of 3 or 5), then checks to see if the number is a multiple of both 3 and 5, then three, then five, then if it number does not fit any of those conditions it prints the number, and finally if the input is not a number greater than or equal to 1 the program lets the user know.

--- Week 3/fizzbuzz.js
+++ Week 3/fizzbuzz.js
@@ -1,1 +1,26 @@
+var num = prompt("Please enter a number", "1");
+var num1 = parseInt(num);
+var checkThree;
+var checkFive;
+var checkBoth;
+if (num1 >= 1) {
+	checkThree = num1%3;
+	checkFive = num1%5;
 
+	if(checkThree == 0 && checkFive == 0){
+		console.log("FizzBuzz");
+	}
+	else if(checkThree == 0){
+		console.log("Fizz");
+	}
+	else if(checkFive == 0){
+		console.log("Buzz");
+	}
+	else{
+		console.log(num);
+	}
+}
+else
+{
+	console.log("Error: invalid input")
+}
